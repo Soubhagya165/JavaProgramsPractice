aa132c01c213a98ca448b4a7bf9fd65cd4f89476
Soubhagya165 <240720100036@centurionuniv.edu.in>
2025-05-09 01:22:43 +0530

    commit

--- Abstractclass.java
+++ Abstractclass.java
@@ -8,14 +8,28 @@ abstract class Main {
     }
 }
 
+class newMain extends Abstractclass {
+    void display1() {
+        System.out.println("This is my second Implementation");
+    }
+
+    void display4 ()
+    {
+        System.out.println("Another Method");
+    }
+}
+
 public class Abstractclass extends Main {
     void display1 () {
         System.out.println("This is implementing abstract classes");
     }
     public static void main(String[] args) {
         Abstractclass obj = new Abstractclass();
+        newMain obj2 = new newMain();
         obj.display1();
-        obj.display2();
-        obj.display3();
+        obj2.display1();
+        obj2.display2();
+        obj2.display3();
+        obj2.display4();
     }
 }
